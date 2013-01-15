75353791b6bae08efa56460c7040e144dfd599e9
Jonathan Pelletier <jonathan.pelletier1@gmail.com>
2013-01-15 17:11:12 -0500

    progress

--- install.sh
+++ install.sh
@@ -2,5 +2,21 @@
 
 # install the monitor component in /usr/bin
 cp ./monitor /usr/bin
+
+# install the repgen component in /usr/bin
+cp ./repgen /usr/bin
+
+# install the monitor config file and template file in /etc/monitor/
+mkdir -p /etc/monitor
+cp ./monitor.conf /etc/monitor
+cp ./monitor.mako /etc/monitor
+
+# create the directory for the log files
+mkdir -p /var/log/monitor
+
+# install the cron job
+crontab -l | sed '/monitor/d' > /tmp/tmp_cron 
+echo "* * * * * /usr/bin/monitor" >> /tmp/tmp_cron 
+crontab < /tmp/tmp_cron
 exit
 
